f276fa4445e23119df029df5e96a82af675eab9a
Igor Bondarenko <bondarenko200021@mail.ru>
2024-09-30 17:06:48 +0700

    main file + engine

---
+++ main.py
@@ -0,0 +1,47 @@
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+import config
+from sqlalchemy import create_engine
+from models.base import Base
+from models.user import User
+from models.address import Address
+engine = create_engine(
+    url = config.SQLALCHEMY_URL,
+    echo = config.SQLALCHEMY_ECHO,
+)
+
+#Контекстынй менеджер для работы с сессией БД    
+with Session(engine) as session:
+    #добавление данных в базу, согласно нашим моделям orm
+    # spongebob = User(
+    #     name="spongebob",
+    #     fullname="Spognebob Squarepants",
+    #     addresses=[Address(email_address="spongebob@sqlalchemy.org")],
+    # )
+    # sandy = User(
+    #     name="sandy",
+    #     fullname="Sandy Cheeks",
+    #     addresses=[
+    #         Address(email_address="sandy@sqlalchemy.org"),
+    #         Address(email_address="sandy@squirrelpower.org"),
+    #     ],
+    # )
+    # patrick = User(
+    #     name="patrick",
+    #     fullname="Patrick Star",
+    # )
+    # #Добавляем наши данные в базу
+    # session.add_all([spongebob, sandy, patrick])
+    # #Сохраняем изменения и закрываем сессию
+    # session.commit()
+    
+    
+    #выборка данных с помощью оператора select
+    session = Session(engine)
+    stmt = select(User).where(User.name.in_(["spongebob", "sandy"]))
+    for user in session.scalars(stmt):
+        print(user)
+    
+Base.metadata.create_all(engine)    
+        
